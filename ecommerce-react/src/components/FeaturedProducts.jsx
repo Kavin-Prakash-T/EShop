1057797f687c8168acc77519e65c4c3fcb698a55
Kavin-Prakash-T2024cse <tkavinprakash@gmail.com>
2025-12-18 19:46:34 +0530

    bug fix

--- ecommerce-react/src/components/FeaturedProducts.jsx
+++ ecommerce-react/src/components/FeaturedProducts.jsx
@@ -8,7 +8,7 @@ const FeaturedProducts = () => {
   useEffect(() => {
     const fetchData = async () => {
       const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/products`);
-      setProducts(res.data);
+      setProducts(res.data||[]);
     };
     fetchData();
   }, []);
